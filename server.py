ca2b0c66f23e646326ef5f2ffdf674bd32ca215b
rscee06-bit <rscee06@gmail.com>
2025-08-22 14:58:59 -0500

    Update server.py

--- server.py
+++ server.py
@@ -506,9 +506,5 @@ app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
