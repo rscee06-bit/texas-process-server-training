bd3d77fb13ad7946a7a60d474a7b91f1f0f225e8
rscee06-bit <rscee06@gmail.com>
2025-08-22 15:10:25 -0500

    Update server.py

--- server.py
+++ server.py
@@ -508,8 +508,3 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
